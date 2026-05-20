b2eaed7a3a4098d5439912486922f6c7b90a9d22
Fyrope83 <0502741@schoolsnet.act.edu.au>
2026-05-20 12:01:36 +1000

    Settings menu fully complete

--- Settings/tabs/Scripts/Save.gd
+++ Settings/tabs/Scripts/Save.gd
@@ -4,11 +4,13 @@ const SAVEFILE = "user://Settings.save"
 
 @onready var X_Mouse_sens_Multi = 0.01
 @onready var Y_Mouse_sens_Multi = 0.01
-
+@onready var Vsync_mode
 @onready var game_data = {}
 
 func _ready():
 	load_data()
+	X_Mouse_sens_Multi = game_data.X_Mouse_sens_Multi
+	Y_Mouse_sens_Multi = game_data.Y_Mouse_sens_Multi
 
 func load_data():
 	var file = FileAccess.open(SAVEFILE, FileAccess.READ)
@@ -59,3 +61,11 @@ func Y_update_mouse_sens(value):
 	game_data.Y_Mouse_sens_Multi = value
 	print(game_data.Y_Mouse_sens_Multi)
 	save_data()
+
+func Update_Vsync():
+	print(game_data.Vsync_on)
+	save_data()
+	if game_data.Vsync_on == true:
+		toggle_vsync(1)
+	elif game_data.Vsync_on == false:
+		toggle_vsync(0)
